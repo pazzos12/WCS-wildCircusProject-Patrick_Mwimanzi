50ec3447dbe9ce821cdced2b53813f5f077f475c
pazzos12 <56323301+pazzos12@users.noreply.github.com>
2020-01-31 09:44:00 +0100

    Add files via upload

---
+++ src/main/java/com/WildCodeSchool/wildcircus/WcsWildCircusProjectPatrickMwimanziApplication.java
@@ -0,0 +1,13 @@
+package com.WildCodeSchool.wildcircus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcsWildCircusProjectPatrickMwimanziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcsWildCircusProjectPatrickMwimanziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WildCodeSchool/wildcircus/WcsWildCircusProjectPatrickMwimanziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WildCodeSchool.wildcircus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcsWildCircusProjectPatrickMwimanziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
